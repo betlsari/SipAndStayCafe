fc5c78c47a56cbb3b109881a604c16aeb36d0462
Ece Akın <akinecee3535@hotmail.com>
2026-05-01 15:22:15 +0300

    tailwind hataar

---
+++ cafeorder-frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: { extend: {} },
+    plugins: [],
+}
